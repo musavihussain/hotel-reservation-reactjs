c076551fb89d0c74b385f8c9154d21dab3b8f2a8
Hussain Musavi <musavihussain@gmail.com>
2020-05-27 21:25:12 +0530

    Banner, Hero, Error, Home, Rooms added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 function Error() {
     return (
-        <div>
-            error page
-        </div>
+        <Hero>
+            <Banner title="404" subtitle="Page not found">
+            <Link to="/" className="btn-primary">Return Home</Link>
+            </Banner>
+            </Hero>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            Home page
-        </div>
-    )
+        <Hero>
+            <Banner title="Hotel Reservation 5 Stars" subtitle="Room starting at $55">
+            <Link to="/rooms" className="btn-primary">Our Rooms</Link>
+            </Banner>
+        </Hero>         
+        
+    )   
 }
 
 export default Home
