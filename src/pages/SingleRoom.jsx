2bb947f5d1c43d1d4a3b7e6578748948bb4bbb91
Hussain Musavi <musavihussain@gmail.com>
2020-05-29 22:13:19 +0530

    SingleRoom details completed

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -33,6 +33,7 @@ export default class SingleRoom extends Component {
         const {name, description, capacity, size, price, extras, breakfast, pets, images} = room
 
         return (
+            <>
             <StyledHero img={images[0] || defaultBcg }>
                 <Banner title={`${name} room`}>
                     <Link to="/rooms" className="btn-primary">
@@ -40,6 +41,36 @@ export default class SingleRoom extends Component {
                     </Link>
                 </Banner>
             </StyledHero>
+            <section className="single-room">
+                <div className="single-room-images">
+                    {images.map((item,index) => {
+                        return <img key={index} src={item} alt={name}/>
+                    })}
+                </div>
+                <div className="single-room-info">
+                    <article className="desc">
+                        <h3>details </h3>
+                <p>{description}</p>
+                    </article>
+                    <article className="info">
+                        <h3>info</h3>
+                <h6>price: ${price}</h6>
+                <h6>size: {size} SQFT</h6>
+                <h6>max capacity: {capacity > 1 ? `${capacity} people` : `${capacity} person`}</h6>
+                <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+                <h6>{breakfast && "free breakfast included"}</h6>
+                    </article>
+                </div>
+            </section>
+            <section className="room-extras">
+                <h6>extras</h6>
+                <ul className="extras">
+                    {extras.map((item, index) => {
+                        return <li key={index}>- {item}</li>; 
+                    })}
+                </ul>
+            </section>
+            </>
         )
     }
 }
